e9281860d764928686dbe2b33df613602a50a62d
Bayu Samudra <bayusamudra.com@outlook.com>
2018-06-30 19:57:18 +0700

    #CHANGE 1 - Import some library from Nuget - Setting up "Netizener" with MahApps.Metro - Make "About" form and add BtnOK Click Handler

---
+++ Netizener/About.xaml.vb
@@ -0,0 +1,18 @@
+﻿Imports MahApps.Metro.Controls
+
+Public Class AboutClass
+	Inherits MetroWindow
+
+	Public Sub New()
+
+		' This call is required by the designer.
+		InitializeComponent()
+
+		' Add any initialization after the InitializeComponent() call.
+
+	End Sub
+
+	Private Sub BtnOK_Click(sender As Object, e As RoutedEventArgs) Handles BtnOK.Click
+		Me.Close()
+	End Sub
+End Class
