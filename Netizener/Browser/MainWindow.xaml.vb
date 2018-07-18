0655a067790a0c7b587100fadb45b9fa1c33eb0e
Bayu Samudra <bayusamudra.55.02.com@gmail.com>
2018-07-19 06:03:08 +0700

    SNAPSHOT# ALPHA2

---
+++ Netizener/Browser/MainWindow.xaml.vb
@@ -0,0 +1,30 @@
+﻿Imports Dragablz
+Imports Netizeners
+
+Public Class MainWindow
+	Dim i As Integer = 1
+	Public Sub New()
+
+		' This call is required by the designer.
+		InitializeComponent()
+
+		' Add any initialization after the InitializeComponent() call.
+		Dim TabItemPlus As New TabItem
+		TabItemPlus.Header = "+"
+
+		TabControl.Items.Add(TabItemPlus)
+
+		Dim TabItem As New TabItem
+		TabItem.Header = "Tab 1"
+		Dim Frame As New Frame
+		Dim frm As Page = New BrowserTmp
+		Frame.Content = frm
+		TabItem.Content = Frame
+
+		TabControl.Items.Add(TabItem)
+	End Sub
+
+	Private Sub TabData_SelectionChanged(sender As Object, e As SelectionChangedEventArgs)
+
+	End Sub
+End Class
--- Netizener/System/Application.xaml.vb
+++ Netizener/System/Application.xaml.vb
@@ -6,9 +6,9 @@ Class Application
 	' can be handled in this file.
 
 	Public Sub New()
-		If My.Computer.Registry.GetValue("HKEY_CURRENT_USER\Software\Netizener", "IsFirstInstall", 1) = 1 Then
+		If My.Computer.Registry.GetValue("HKEY_CURRENT_USER\Software\Netizener", "IsNotFirstInstall", 0) = 0 Then
 			Call FirstInstall()
-			My.Computer.Registry.SetValue("HKEY_CURRENT_USER\Software\Netizener", "IsFirstInstall", 0)
+			My.Computer.Registry.SetValue("HKEY_CURRENT_USER\Software\Netizener", "IsNotFirstInstall", 1)
 			Call InitAll()
 		Else
 			Call InitAll()
